b4bbb04a0d240d0814be634a71ea0f8b80a62ef0
SparrowBo <574202693@qq.com>
2020-08-04 16:16:52 +0800

    add ribbon for consumer-movie

--- microservice-consumer-movie/src/main/java/com/pbb/microserviceconsumermovie/controller/MovieController.java
+++ microservice-consumer-movie/src/main/java/com/pbb/microserviceconsumermovie/controller/MovieController.java
@@ -1,9 +1,12 @@
 package com.pbb.microserviceconsumermovie.controller;
 
 import com.pbb.microserviceconsumermovie.DO.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +14,11 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+
 @RestController
 public class MovieController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MovieController.class);
 
     @Autowired
     private RestTemplate restTemplate;
@@ -20,9 +26,21 @@ public class MovieController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
     @GetMapping("/student/{id}")
     public Student findById(@PathVariable Long id) {
-        return restTemplate.getForObject("http://localhost:8000/" + id, Student.class);
+        // 当 eureka 和 ribbon 一起用时，可以用微服务的虚拟主机名！
+        return restTemplate.getForObject("http://microservice-provider-user/" + id, Student.class);
+    }
+
+    @GetMapping("/log-instance")
+    public void logUserInstance() {
+        ServiceInstance serviceInstance = loadBalancerClient.choose("microservice-provider-user");
+        // 打印当前选择的是哪个节点？
+        MovieController.LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(),
+                serviceInstance.getHost(), serviceInstance.getPort());
     }
 
     /**
